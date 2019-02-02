6a565f26e0918803c5c733c161b559f56f41ebf2
jraylan <jeffersonraylan@gmail.com>
2019-02-02 11:58:57 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 cd /opt/webchat/app
-export TZ=America/Sao_Paulo
-export LC_ALL= pt_BR.UTF-8
-export LC_CTYPE=pt_BR.UTF-8
-export LANG=pt_BR.UTF-8
 export DISPLAY=:1
 export DISPLAY_WIDTH=1024
 export DISPLAY_HEIGHT=768
-export VNCPASSWORD=GQQMBP3NLYWBL6CQHY2Y
-export PATH=/opt/webchat/bin:$PATH
 
 screen -d -m bash -c /usr/bin/awesome&
-/usr/bin/xterm -e "/opt/webchat/python/bin/python manage.pyc whatsapp"&
-/usr/bin/xterm -e "/opt/webchat/python/bin/python manage.pyc facebook"&
